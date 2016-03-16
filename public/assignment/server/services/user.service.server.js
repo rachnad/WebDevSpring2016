2e5bb4059f70fafe3496b871b537b8423c5e75d3
Rachana <deshmukh.r@husky.neu.edu>
2016-03-15 23:28:50 -0400

    HTTP endpoints for User Service

---
+++ public/assignment/server/services/user.service.server.js
@@ -0,0 +1,57 @@
+/**
+ * Created by rachanadeshmukh on 3/15/16.
+ */
+
+module.exports = function(app, model) {
+    app.post("/api/assignment/user", createUser);
+    app.get("/api/assignment/user", getUsers);
+    app.get("/api/assignment/user/:id", getUserbyId);
+    app.get("/api/assignment/user?username=alice&password=wonderland", getAlice);
+    app.get("/api/assignment/user?username=username", getUserbyUsername);
+    app.put("/api/assignment/user/:id", updateUser);
+    app.delete("/api/assignment/user/:id", deleteUser);
+
+
+    var model = model;
+
+    function createUser(req, res) {
+        var newUser = req.body;
+        res.json(model.createUser(newUser));
+    }
+
+    function getUsers(req, res) {
+        model.findAll();
+    }
+
+    function getUserbyId(req, res) {
+        var id = req.params._id;
+        var user = model.findUserById(id);
+        res.json(user);
+    }
+
+    function getUserbyUsername(req, res) {
+        var username = req.params.username;
+        var user = model.findUserByUsername(username)
+        res.json(user);
+    }
+
+    function getAlice(req, res) {
+        var alice = req.params.firstName;
+        var wonderland = req.params.lastName
+        var user = model.findUserById(id);
+        res.json(user);
+    }
+
+    function updateUser(req, res) {
+        var userid = req.params.id;
+        var updatedbody = req.body;
+        updatedbody._id = userid;
+        res.json(model.updateUser(updatedbody));
+    }
+
+
+    function deleteUser(req, res) {
+        var userid = req.params.id;
+        res.json(model.deleteUserById(userid));
+    }
+}
